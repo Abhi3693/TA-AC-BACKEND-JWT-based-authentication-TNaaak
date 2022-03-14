a0c341a4bb5afd134dc048691fc416ee36c6625b
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-03-14 22:55:04 +0530

    Added change in assignment-3 of jwt-authorization in api

--- block-BNaaen/Article/middlewares/auth.js
+++ block-BNaaen/Article/middlewares/auth.js
@@ -1,20 +1,37 @@
 var jwt = require('jsonwebtoken');
 
 module.exports = {
-  verifyUser : async (req, res, next) => {
+  verifyUser: async (req, res, next) => {
     let token = req.headers.authorization;
     try {
       let payLoad = await jwt.verify(token, process.env.SECRET);
-      if(payLoad) {
+      if (payLoad) {
         req.user = payLoad;
         next();
       } else {
-        res.status(400).json({error: ["token required"]});
+        res.status(401).json({ error: ['token required'] });
       }
     } catch (error) {
-      res.status(400).json({error: [error]});
+      res.status(401).json({ error: [error] });
+    }
+  },
+  optionalUser: async (req, res, next) => {
+    let token = req.headers.authorization;
+    if (token) {
+      try {
+        let payLoad = await jwt.verify(token, process.env.SECRET);
+        if (payLoad) {
+          req.user = payLoad;
+          next();
+        } else {
+          res.status(401).json({ error: ['token required'] });
+        }
+      } catch (error) {
+        res.status(401).json({ error: [error] });
+      }
+    } else {
+      req.user = null;
       next();
     }
-    
-  }
-}
+  },
+};
